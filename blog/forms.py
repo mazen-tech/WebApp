f8ce028860325a4ea4a74d65ca1e237dc4692850
Kristina <118684323+nedkrss@users.noreply.github.com>
2025-06-08 10:04:30 +0200

    added functions to like, unlike, comment

---
+++ blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Comment
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'rows': 3,
+                'class': 'form-control',
+                'placeholder': 'Write your comment here...'
+            }),
+        }
+        
--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
 	content = models.TextField()
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
 
 	def __str__(self):
 		return self.title
@@ -21,4 +22,13 @@ class Post(models.Model):
 	def get_absolute_url(self):
 		return reverse('post-detail', kwargs={'pk': self.pk})
 
-        
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment from {self.author.username} to post "{self.post.title}"'
